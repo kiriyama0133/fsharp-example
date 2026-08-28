1a67ddd3cadbdf2f86712778ec88a3c03af1abb5
kiriyama0133 <2074288854@qq.com>
2026-08-28 17:41:25 +0800

    feat: implement Point2D and Point3D types

--- models/Vec.fsx
+++ models/Vec.fsx
@@ -1,3 +1,5 @@
+open System
+
 [<Struct>]
 [<CustomEquality>]
 [<CustomComparison>]
@@ -26,3 +28,41 @@ type Point2D =
                 let len2 = p.Length()
                 compare len1 len2
             | _ -> invalidArg "other" "Must be a Point"
+
+[<Struct>]
+[<CustomEquality>]
+[<CustomComparison>]
+type Point3D =
+    { x: float
+      y: float
+      z: float }
+
+    static member (+)(p1: Point3D, p2: Point3D) =
+        { x = p1.x + p2.x
+          y = p1.y + p2.y
+          z = p1.z + p2.z }
+
+    static member (-)(p1: Point3D, p2: Point3D) =
+        { x = p1.x - p2.x
+          y = p1.y - p2.y
+          z = p1.z - p2.z }
+
+    member this.Length() =
+        sqrt (this.x * this.x + this.y * this.y + this.z * this.z)
+
+    override this.Equals(obj: obj) : bool =
+        match obj with
+        | :? Point3D as p -> this.Length() = p.Length()
+        | _ -> false
+
+    override this.GetHashCode() : int = this.Length().GetHashCode()
+
+    interface System.IComparable with
+        member this.CompareTo(obj: obj) : int =
+            match obj with
+            | :? Point2D as p -> raise (ArgumentException "type p must be Point3D but Point2D")
+            | :? Point3D as p ->
+                let len1 = this.Length()
+                let len2 = p.Length()
+                compare len1 len2
+            | _ -> raise (ArgumentException("Expected Point3D", "obj"))
