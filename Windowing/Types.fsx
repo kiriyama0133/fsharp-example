1c3aa2f4f43c52fae3e05a7dbfb83833f82546b9
kiriyama0133 <2074288854@qq.com>
2026-08-30 16:46:12 +0800

    feat: implement window types

---
+++ Windowing/Types.fsx
@@ -0,0 +1,123 @@
+module WindowTypes =
+
+    open System
+
+    type WindowId = WindowId of int
+    type PointerId = PointerId of int
+
+    type EventContext =
+        { WindowId: WindowId
+          Timestamp: int64
+          mutable Handled: bool
+          mutable Cancel: bool }
+
+    type WindowState =
+        | Created
+        | Visible
+        | Hidden
+        | Closing
+        | Closed
+
+    type FocusState =
+        | Focused
+        | Unfocused
+
+    type ActivationState =
+        | Active
+        | Inactive
+
+    type PointerButton =
+        | Left
+        | Right
+        | Middle
+        | X1
+        | X2
+
+    type WindowBounds =
+        { X: float
+          Y: float
+          Width: float
+          Height: float }
+
+    type WindowStyle =
+        { Resizable: bool
+          Borderless: bool
+          Topmost: bool
+          Transparent: bool
+          AcceptsInput: bool }
+
+    type PointerPosition = { X: float; Y: float }
+
+    type PointerDelta = { X: float; Y: float }
+
+    type PointerState =
+        { Position: PointerPosition
+          IsInside: bool
+          CapturedBy: WindowId option }
+
+    type WindowEvent =
+        | Shown
+        | Hidden
+        | CloseRequested
+        | Closed
+        | Activated
+        | Deactivated
+        | FocusGained
+        | FocusLost
+        | TitleChanged of string
+        | StyleChanged of WindowStyle
+        | Moved of WindowBounds
+        | Resized of WindowBounds
+
+    type PointerEventKind =
+        | Entered
+        | Exited
+        | Moved
+        | Pressed
+        | Released
+        | Wheel
+
+    type KeyboardEventKind =
+        | KeyDown
+        | KeyUp
+        | TextInput
+
+    type PointerEvent =
+        { Context: EventContext
+          PointerId: PointerId
+          Position: PointerPosition
+          Button: PointerButton option
+          Delta: PointerDelta option }
+
+    type KeyboardEvent =
+        { Context: EventContext
+          Key: string option
+          Text: string option
+          Ctrl: bool
+          Shift: bool
+          Alt: bool }
+
+    type WindowMessage =
+        | Window of EventContext * WindowEvent
+        | Pointer of PointerEventKind * PointerEvent
+        | Keyboard of KeyboardEventKind * KeyboardEvent
+
+    let createEventContext windowId : EventContext =
+        { WindowId = windowId
+          Timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()
+          Handled = false
+          Cancel = false }
+
+    let defaultWindowStyle : WindowStyle =
+        { Resizable = true
+          Borderless = false
+          Topmost = false
+          Transparent = false
+          AcceptsInput = true }
+
+    let defaultPointerPosition : PointerPosition = { X = 0.0; Y = 0.0 }
+
+    let defaultPointerState : PointerState =
+        { Position = defaultPointerPosition
+          IsInside = false
+          CapturedBy = None }
