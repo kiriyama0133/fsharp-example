f657a9af1bdbf0f7a43fb44657428049d9feebe0
kiriyama0133 <2074288854@qq.com>
2026-09-01 01:01:20 +0800

    feat: upgrade Skia/Canvas.fs

--- src/FSharpCore/Graphics/GraphicsTypes.fs
+++ src/FSharpCore/Graphics/GraphicsTypes.fs
@@ -1,33 +1,42 @@
 module GraphicsTypes
 
-    open Vec
+open Vec
 
-    type Color =
-        { R: float
-          G: float
-          B: float
-          A: float }
+type Color =
+    { R: float
+      G: float
+      B: float
+      A: float }
 
-    type Size = { Width: float; Height: float }
+type Size = { Width: float; Height: float }
 
-    type Rect = { Position: Point2D; Size: Size }
+type Rect = { Position: Point2D; Size: Size }
 
-    type ImageId = ImageId of int
+type ImageId = ImageId of int
 
-    type FontId = FontId of int
+type FontId = FontId of int
 
-    type Image = { Id: ImageId; Size: Size }
+type Image = { Id: ImageId; Size: Size }
+type Brush = SolidColor of Color
+type Sroke = { Color: Color; Width: float }
 
-    type Font =
-        { Id: FontId
-          Family: string
-          Size: float }
+type Canvas =
+    { Clear: Color -> unit
+      DrawRect: Rect -> uint
+      DrawLine: Point2D -> Point2D -> Sroke -> unit
+      DrawImage: Image -> Rect -> unit }
 
-    type RenderTarget =
-        | WindowTarget
-        | TextureTarget of ImageId
 
-    type Primitive =
-        | Rectangle of Rect * Color
-        | Image of ImageId * Rect
-        | Text of string * FontId * Point2D * Color
+type Font =
+    { Id: FontId
+      Family: string
+      Size: float }
+
+type RenderTarget =
+    | WindowTarget
+    | TextureTarget of ImageId
+
+type Primitive =
+    | Rectangle of Rect * Color
+    | Image of ImageId * Rect
+    | Text of string * FontId * Point2D * Color
---
+++ src/FSharpCore/Graphics/Skia/Canvas.fs
@@ -0,0 +1,11 @@
+module Graphics.Skia.Canvas
+
+open GraphicsTypes
+open Vec
+
+type Canvas =
+    { Clear: Color -> unit
+      DrawRect: Rect -> Brush -> unit
+      DrawLine: Point2D -> Point2D -> Stroke -> unit
+      DrawImage: Image -> Rect -> unit
+      DrawText: string -> Point2D -> Font -> Color -> unit }
